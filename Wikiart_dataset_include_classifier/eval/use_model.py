a01a18b16520c143bc125f96b274e8856e2b400d
Xingyu Lai <laixingyu_2023@qq.com>
2025-03-24 20:19:28 +0800

    Update use_model.py

--- Wikiart_dataset_include_classifier/eval/use_model.py
+++ Wikiart_dataset_include_classifier/eval/use_model.py
@@ -7,35 +7,20 @@ import torch
 from torchvision import transforms
 from Wikiart_dataset_include_classifier.models.DualPathResNet18_UNet_without_SEblock import DualPathResNet18_UNet
 from PIL import Image
-
-<<<<<<< HEAD
-# 加载模型
-model= DualPathResNet18_UNet(num_classes=11)
-checkpoint = torch.load('../train_models/checkpoint/DualPathResNet18-UNet_lr0.03_sgd_batchsize64_epochs50_weight_decay0.0007.pth', map_location=torch.device('cpu'),weights_only=True)
-model.load_state_dict(checkpoint['model_state_dict'])
-# 预处理图像
-image = Image.open('test.jpg')
-transform = transforms.Compose([
-                transforms.Resize((256, 256)),
-                transforms.ToTensor(),
-                transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-            ])  # 与训练时相同的预处理
-input_tensor = transform(image).unsqueeze(0)  # 增加batch维度
-=======
 def classify(image_path):
+
     # 加载模型
     model= DualPathResNet18_UNet(num_classes=11)
-    checkpoint = torch.load('Wikiart_dataset_include_classifier/train_models/checkpoint/DualPathResNet18-UNet_lr0.03_sgd_batchsize64_epochs50_weight_decay0.00015.pth', map_location=torch.device('cpu'),weights_only=True)
+    checkpoint = torch.load('../train_models/checkpoint/DualPathResNet18-UNet_lr0.03_sgd_batchsize64_epochs50_weight_decay0.0007.pth', map_location=torch.device('cpu'),weights_only=True)
     model.load_state_dict(checkpoint['model_state_dict'])
     # 预处理图像
     image = Image.open('image_path')
     transform = transforms.Compose([
-                    transforms.Resize((256, 256)),
-                    transforms.ToTensor(),
-                    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-                ])  # 与训练时相同的预处理
+                transforms.Resize((256, 256)),
+                transforms.ToTensor(),
+                transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+            ])  # 与训练时相同的预处理
     input_tensor = transform(image).unsqueeze(0)  # 增加batch维度
->>>>>>> 026f7d8f46f0ec2d8c576d615228668b3148cb5e
 
     # 推理
     model.eval()
